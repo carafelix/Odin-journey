dfd89ed5ba7bd0e66e838aea46a571a37e1547ca
carafelix <heroprotagonist32@gmail.com>
2023-06-05 16:31:52 -0400

    Test

---
+++ js/testing-site/test.js
@@ -0,0 +1,47 @@
+// console.log ('Hi, how are you?')
+
+// // let message = 'Hello!', // define the variable and assign the value, coma (+ start for readibility) multiple assign, could also repeat let ;
+// // user = 'John',
+// // age = 25;
+
+// let hello = 'Hello world!';
+
+// let message;
+
+// // copy 'Hello world' from hello into message
+// message = hello;
+
+// // now two variables hold the same data
+// alert(message); // Hello world!
+
+// // Declare two variables: admin and name.
+// let admin;
+// // Assign the value "John" to name.
+// let name = "Jhon";
+// // Copy the value from name to admin.
+// admin = name;
+// // Show the value of admin using alert (must output “John”).
+// alert(admin);
+
+
+
+
+// //Create a variable with the name of our planet. How would you name such a variable?
+
+// let planetEarth;
+
+// // Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+// let currentUserName;
+
+// const myInt = 5;
+// const myFloat = 6.667;
+// myInt;
+// myFloat;
+
+// let a = 10
+// let b = 7 * a
+
+
+
+console.log();
