5f8dc7014c69f97baa769b15da28dddf2e4e91e3
carafelix <heroprotagonist32@gmail.com>
2023-06-13 16:55:22 -0400

    Add comments on functions and solve ex

--- js/testing-site/functions.js
+++ js/testing-site/functions.js
@@ -6,6 +6,8 @@ function hello(name = "Veet") {
     alert(`Soy el famoso numero ${matematiks} jajajaja!`);
   }
 
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Functions#anonymous_function_example
+
   (function () {
     alert("hello");
   });
@@ -16,3 +18,44 @@ function hello(name = "Veet") {
 
 // Note: This form of creating a function is also known as function expression.
 //  Unlike function declaration, function expressions are not hoisted.
+
+let userName = 'John';
+
+function showMessage() {
+  userName = "Bob"; // (1) changed the outer variable
+
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+alert( userName ); // John before the function call
+
+showMessage();
+
+alert( userName ); // Bob, the value was modified by the function
+
+// If a same-named variable is declared inside the function then it shadows the outer one.
+// For instance, in the code below the function uses the local userName. The outer one is ignored: https://javascript.info/function-basics#outer-variables
+
+// One additional thing. You can also 'modify in place' with the nullish coalescing operator, like ++ or so:
+
+// let num = null
+// console.log(num??=10)
+// //10
+
+// If you console.log(num) again, you get 10.
+// So the value of num has been modified in place.
+
+// let int = 1
+// console.log(int??=10)
+// //1
+
+// Here the value of int is not modified, it remains 1, because it’s neither null nor undefined. 
+
+function min(a, b) {
+  if (a>b) {
+    return b
+  } else {
+    return a
+  }
+}
