d685325365429693d4ec3a711ffb1ee902b24a58
carafelix <heroprotagonist32@gmail.com>
2023-06-20 17:19:35 -0400

    initial commit

---
+++ js/testing-site/mdn-active.js
@@ -0,0 +1,10 @@
+let output = document.querySelector('.output');
+output.innerHTML = '';
+
+
+
+const para = document.createElement('p');
+para.textContent = ;
+output.appendChild(para);
+
+for let i = 10;
