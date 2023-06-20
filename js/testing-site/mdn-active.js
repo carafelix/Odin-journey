dc90e58ddb56347640de2974a95dc34490bd247e
carafelix <heroprotagonist32@gmail.com>
2023-06-20 17:29:48 -0400

    Countdown ex

--- js/testing-site/mdn-active.js
+++ js/testing-site/mdn-active.js
@@ -1,10 +1,15 @@
 let output = document.querySelector('.output');
 output.innerHTML = '';
 
+for (let i = 10; i>=0; i--) {
+    const para = document.createElement('p');
+    output.appendChild(para);
+    if (i == 10) {
+        para.textContent = `Initilize Countdown... ${i}`;
+    } else if (i == 0) {
+        para.textContent = `${i} KABOOOM!`;
 
-
-const para = document.createElement('p');
-para.textContent = ;
-output.appendChild(para);
-
-for let i = 10;
+    } else {
+        para.textContent = `${i}`;
+    }
+}
