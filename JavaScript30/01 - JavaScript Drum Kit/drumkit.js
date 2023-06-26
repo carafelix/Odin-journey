1057f13287da207ea84728bfb5fc7bf93f2ad1b2
carafelix <heroprotagonist32@gmail.com>
2023-06-26 19:51:23 -0400

    add comments

--- JavaScript30/01 - JavaScript Drum Kit/drumkit.js
+++ JavaScript30/01 - JavaScript Drum Kit/drumkit.js
@@ -1,27 +1,27 @@
-window.addEventListener(
-    'keydown', (e) => {
-        console.log(e.code)  // .key !== .code
-    }
-);
-
+// set data-keys via js
 
 const keys = document.querySelectorAll('.key');
 const audios = document.querySelectorAll('audio')
 
 const audioKey = [];
 
+function getIndex() {
+    return audioKey.shift() // each time the function is called it returns the shifted value
+}
+
 keys.forEach(key => {
     key.setAttribute('data-key', `Key${key.firstElementChild.innerText}`)
-    audioKey.push(key.firstElementChild.innerText)
+    audioKey.push(key.firstElementChild.innerText)  // set up array with all letters
 });
-
-function getIndex() {
-    return audioKey.shift()
-}
 
 audios.forEach(audio => {
     audio.setAttribute('data-key', `Key${getIndex()}`)
 });
 
+// window
 
-console.log(audioKey);
+window.addEventListener(
+    'keydown', (e) => {
+        console.log(e.code)  // .key !== .code
+    }
+);
