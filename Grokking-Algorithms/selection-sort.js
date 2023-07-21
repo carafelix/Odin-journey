643cc09cf6520393a049e7b33356d22d9e68f5bd
carafelix <heroprotagonist32@gmail.com>
2023-07-21 16:33:29 -0400

    selection sort from scrap pre-read

--- Grokking-Algorithms/selection-sort.js
+++ Grokking-Algorithms/selection-sort.js
@@ -14,8 +14,35 @@ const wilco = new Artist('wilco',111);
 
 const music = [radiohead, kishoreKumar, theBlackKeys, neutralMilkHotel, beck, theStrokes, wilco];
 
+const alone = [156,141,35,94,88,61,111];
+
+// pre-read
+
 function sortMostListen(arr){
-    arr.reduce((a,b)=>{
-        (b.playCount>a) ? true : false
-    },0)
+
+    let artist = arr;
+    const sorted = [];
+
+
+        for(i = 0; i < arr.length;){
+
+            let index = 0;
+            let current = artist[0]
+
+                for (j = 0; j<artist.length; j++){
+
+                    if(artist[j]>current){
+                        current = artist[j];
+                        index = j
+                    }
+                }
+            sorted.push(artist[index])
+            artist.splice(index,1)
+        
+        }
+
+    return sorted
+
 }
+
+console.log(sortMostListen(alone));
